339e3b575a279e7b28d14648515d654f6dc496ce
MaverickD650 <92877124+MaverickD650@users.noreply.github.com>
2024-12-17 20:50:30 +0000

    try3

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,13 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema'
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader({ extend: (context) => blogSchema(context) }), schema: docsSchema() }),
+	docs: defineCollection({ 
+		loader: docsLoader(),
+		schema: docsSchema({
+			extend: (context) => blogSchema(context)
+		})
+	}),
 };
